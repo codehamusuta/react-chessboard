fe9d46fb26ec1fd0f0d3fd6db495cba7816ce60d
codehamusuta <codehamusuta>
2023-09-24 21:32:54 +0800

    allowed chess pieced to be moved and dropped

--- src/App.js
+++ src/App.js
@@ -14,12 +14,15 @@ function getPieceImg(pieceNotation) {
 const RANKS = [1,2,3,4,5,6,7,8];
 const FILES = ['a','b','c','d','e','f','g','h'];
 
-function ChessPiece({piece}) {
+function ChessPiece({piece, rank, file}) {
   const [isDragging, setIsDragging] = useState(false)
   
   const onDragStart = (e) => {
     setIsDragging(true);
     e.dataTransfer.setData("piece", piece);
+    e.dataTransfer.setData("rank", rank);
+    e.dataTransfer.setData("file", file);
+    
     e.dataTransfer.effectAllowed = "move";
     console.log(e.target);
   }
@@ -39,13 +42,21 @@ function ChessPiece({piece}) {
   )
 }
 
-function Square({rank, file, color, piece}) {
+function Square({rank, file, color, piece, onMove}) {
   const onDragOver = (e) => {
     e.preventDefault();
   }
   const onDrop = (e) => {
     e.preventDefault();
+    
+    onMove(
+      e.dataTransfer.getData("piece"),
+      [e.dataTransfer.getData("rank"), e.dataTransfer.getData("file")],
+      [rank, file]
+    );
+
     console.log(e.dataTransfer.getData("piece"));
+    console.log(rank, file);
   }
   return (
     <div className={"square " + color} 
@@ -54,14 +65,25 @@ function Square({rank, file, color, piece}) {
     >
       <span className="file-text">{rank === 7? FILES[file] : null}</span>
       <span className="rank-text">{file == 0? RANKS[rank] : null}</span>
-      <ChessPiece piece={piece} />
+      <ChessPiece piece={piece} rank={rank} file={file}/>
     </div>
   );
 }
 
 
 
-function ChessBoard({position}) {
+function ChessBoard() {
+  const [position, setPosition] = useState(setBoard())
+
+  function onMove(piece, origin, dest) {
+    const nextPos = position.slice();
+    nextPos[origin[0]][origin[1]] = null;
+    console.log(nextPos);
+    nextPos[dest[0]][dest[1]] = piece;
+    console.log(nextPos);
+    setPosition(nextPos);
+  }
+  
   const board = Array(8).fill(0).map((_, i) => {
     const row = Array(8).fill(0).map((_, j) => {
       return (
@@ -71,6 +93,7 @@ function ChessBoard({position}) {
           file={j} 
           color={(i+j) % 2 === 0? "light":"dark"}
           piece={position[i][j]}
+          onMove={onMove}
         />
       );
     });
@@ -89,7 +112,7 @@ function ChessBoard({position}) {
 
 function setBoard() {    
   // Set the initial position of board to a new game 
-  const position = Array(8).fill(Array(8));
+  const position = new Array(8).fill(null).map(()=> new Array(8).fill(null));
   position[0] = 'rnbqkbnr'.split("");
   position[1] = 'pppppppp'.split("");
   position[position.length - 1] = 'RNBQKBNR'.split("");
@@ -98,9 +121,9 @@ function setBoard() {
 }
 
 function Game() {
-  const [position, setPosition] = useState(setBoard())
+  // const [position, setPosition] = useState(setBoard())
   return (
-    <ChessBoard position={position} />
+    <ChessBoard />
   )
 }
 
